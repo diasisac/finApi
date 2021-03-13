57f1325b186daed8cee4e9ff4f737841cd0299ca
diasisac <isac@mobilesaude.com>
2021-03-13 14:40:20 -0300

    Validando a conta

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const { v4: uuidv4 } = require("uuid")
 
 const app = express();
+app.use(express.json());
 
 const customers = [];
-
 /**
  * cpf - string
  * name - string
@@ -13,10 +14,19 @@ const customers = [];
  * statement []
  */
 
-app.use(express.json());
+
 app.post("/account",(request, response) =>{
     const {cpf, name} = request.body;
     
+    //Verifica se o CPF vindo do body já está cadastrado
+    const customerAlreadyExists = customers.some( 
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists){
+        return response.status(400).json({error:"Customer already exists!"});
+    }
+
     const id = uuidv4();
 
     customers.push({
@@ -27,6 +37,18 @@ app.post("/account",(request, response) =>{
     });
 
     return response.status(201).send();
+});
+
+app.get("/statement/",(request, response) =>{
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(404).json({error:"Custumer not found!"})
+    }
+
+    return response.json(customer.statement);
 });
 
 app.listen(3333);
